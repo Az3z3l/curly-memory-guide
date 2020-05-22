b262e82be6657aa9bf0db6e295dd275ea61f7a94
Vishwaak <chandranvishwaak@gmail.com>
2020-05-23 03:38:54 +0530

    Added process_block::main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <stdio.h>
 #include <vector>
+#include "first_fit.cpp"
 using namespace std;
 int main()
 {
-    vector <int> memory_block;
-    int flag=0,mm_size=0;
+    vector <int> memory_block,process_list;
+    int flag=0,mm_size=0,p_size=0;
     cout<<"Enter the number of blocks : ";
     cin>>mm_size;
     while(mm_size > 0)
@@ -16,6 +17,22 @@ int main()
         memory_block.push_back(inp);
         mm_size--;
     }
+    cout<<"Enter the number of process : ";
+    cin>>p_size;
+    flag=0;
+    while(p_size > 0)
+    {   
+        int inp;
+        cout<<"Enter the process size of  P"<<flag++<<" size : ";
+        cin>>inp;
+        process_list.push_back(inp);
+        p_size--;
+    }
 
+    vector<int> minewine = first_fit(memory_block,process_list);
+    for(int o=0; o<minewine.size(); o++)
+    {
+        cout<<minewine[o]<<endl;
+    }
     return 0;
 }
