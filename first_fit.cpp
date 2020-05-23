5918362f95ded11dd8e435ba37c13c288ae76917
Vishwaak <chandranvishwaak@gmail.com>
2020-05-23 17:54:13 +0530

    Update::first_fit.cpp

--- first_fit.cpp
+++ first_fit.cpp
@@ -5,10 +5,8 @@
 using namespace std;
 
 vector <int> first_fit(vector <int> memory_blocks, vector <int> process_queue){
-    int memory_job[memory_blocks.size()];
-    memory_job[memory_blocks.size()] = {0};
-    int memory_status[memory_blocks.size()];
-    for(int k=0; k< memory_blocks.size(); k++){ memory_status[k]=0;}
+    int memory_job[memory_blocks.size()],memory_status[memory_blocks.size()];
+    for(int k=0; k< memory_blocks.size(); k++){ memory_status[k]=0; memory_job[k]=-100;}
     for(int i=0; i<process_queue.size(); i++)
     {   
         for(int j=0; j<memory_blocks.size(); j++)
