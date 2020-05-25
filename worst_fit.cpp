d955a9c174bb8d592e362676855741ccfcdf987c
Vishwaak <chandranvishwaak@gmail.com>
2020-05-26 03:26:07 +0530

    Bug_Fix::worst_fit.cpp

--- worst_fit.cpp
+++ worst_fit.cpp
@@ -5,10 +5,11 @@
 using namespace std;
 
 vector <int> worst_fit(vector <int> memory_blocks, vector <int> process_queue){
-int memory_job[memory_blocks.size()];
+int memory_job[memory_blocks.size()],memory_status[memory_blocks.size()];
     for(int k=0; k< memory_blocks.size(); k++)
     { 
         memory_job[k]=-100;
+        memory_status[k]=0;
     }
 
     for(int i=0; i<process_queue.size(); i++)
@@ -16,11 +17,12 @@ int memory_job[memory_blocks.size()];
         int maximum = -222; 
         for(int j=0; j<memory_blocks.size(); j++)
         {
-            if(memory_blocks[j] >= process_queue[i])
+            if((memory_blocks[j] >= process_queue[i])&& (memory_status[j] == 0))
             { 
                 if (maximum == -222) 
                 {
                     maximum = j; 
+                    
                 }
                 else if (memory_blocks[maximum] < memory_blocks[j]) 
                 {    
@@ -29,7 +31,8 @@ int memory_job[memory_blocks.size()];
             } 
         } 
         if (maximum != -222) 
-        {
+        {   
+            memory_status[maximum] = 1;
             memory_job[maximum] = i;
             memory_blocks[maximum] -= process_queue[i]; 
         } 
