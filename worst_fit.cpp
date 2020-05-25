bc00b9b1f81230a14e9b0aa10506bbfceac7d50c
Az3z3l <yaswant.yme@gmail.com>
2020-05-25 18:40:01 +0530

    Update:: working now

--- worst_fit.cpp
+++ worst_fit.cpp
@@ -5,24 +5,33 @@
 using namespace std;
 
 vector <int> worst_fit(vector <int> memory_blocks, vector <int> process_queue){
-int memory_job[memory_blocks.size()],memory_status[memory_blocks.size()];
-    for(int k=0; k< memory_blocks.size(); k++){ memory_status[k]=0; memory_job[k]=-100;}
+int memory_job[memory_blocks.size()];
+    for(int k=0; k< memory_blocks.size(); k++)
+    { 
+        memory_job[k]=-100;
+    }
+
     for(int i=0; i<process_queue.size(); i++)
     {   
-        int wstIdx = -1; 
+        int maximum = -222; 
         for(int j=0; j<memory_blocks.size(); j++)
         {
-        if((memory_blocks[j] >= process_queue[i])&& (memory_status[j] == 0))
+            if(memory_blocks[j] >= process_queue[i])
             { 
-                if (wstIdx == -1) 
-                    wstIdx = j; 
-                else if (memory_blocks[wstIdx] < memory_blocks[j]) 
-                    wstIdx = j; 
+                if (maximum == -222) 
+                {
+                    maximum = j; 
+                }
+                else if (memory_blocks[maximum] < memory_blocks[j]) 
+                {    
+                    maximum = j; 
+                }    
             } 
         } 
-        if (wstIdx != -1) 
-        {      
-         memory_blocks[wstIdx] -= process_queue[i]; 
+        if (maximum != -222) 
+        {
+            memory_job[maximum] = i;
+            memory_blocks[maximum] -= process_queue[i]; 
         } 
     }
      vector <int> temp(memory_job,memory_job +  memory_blocks.size());
